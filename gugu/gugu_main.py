54c1f9809d5a6945b2daa8db3bc1d9d38c29a07d
link0233 <054656-106144@stu.mlc.edu.tw>
2021-11-27 09:57:05 +0800

    完成建立標籤城市

--- gugu/gugu_main.py
+++ gugu/gugu_main.py
@@ -9,3 +9,15 @@ class gugu(Canvas):
     
     def mainloopp(self):
         self.root.mainloop()
+
+    def newRec(self,Position,color):
+        return self.create_rectangle(Position[0],Position[1],Position[2],Position[3],fill=color)
+
+    def newText(self,position,_text,color,size):
+        return self.create_text(position[0],position[1],text=_text,fill=color,font=('Arial',size))
+    
+    def newLabel(self,position,text,bgcolor,color,size,width=0,height=0):
+        if bgcolor!=None:
+            self.position=[position[0]-width,position[1]-height,position[0]+width,position[1]+height]
+            self.newRec(self.position,bgcolor)
+        self.newText(position,text,color,size)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from gugu.gugu_main import gugu
 
 class main(gugu):
     def __init__(self):
-        super(main,self).__init__("射飛機","#000000",640,480)
+        super(main,self).__init__("射飛機","skyblue",640,480)
+        self.newLabel((50,100),"hello",None,'#ffffff',12,width=25,height=30)
         self.mainloopp()
 
 if __name__=='__main__':
