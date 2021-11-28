43b0db40d71639c20897ef4fc9660671d2522586
link0233 <054656-106144@stu.mlc.edu.tw>
2021-11-28 08:57:38 +0800

    完成標題

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from gugu.gugu_main import gugu
+from main_screen.title import mainScreenTitle
 
 class main(gugu):
     def __init__(self):
         super(main,self).__init__("射飛機","skyblue",640,480)
-        self.newLabel((50,100),"hello",None,'#ffffff',12,width=25,height=30)
+        self.mainScreenStart()
         self.mainloopp()
+
+    def mainScreenStart(self):
+        self.mainScreenTitle=mainScreenTitle(self)
 
 if __name__=='__main__':
     main()
---
+++ main_screen/title.py
@@ -0,0 +1,4 @@
+class mainScreenTitle:
+    def __init__(self,gugu):
+        self.gugu=gugu
+        self.gugu.newLabel((320,100),"射飛機",None,'#ffffff',50)
