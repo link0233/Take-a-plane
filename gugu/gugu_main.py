4cc6390c4220610fa0db6979dd56973cede95d7d
link0233 <054656-106144@stu.mlc.edu.tw>
2021-11-28 20:08:50 +0800

    格式改寫(包含debug)
    日後使用功能比較方便

--- gugu/gugu_main.py
+++ gugu/gugu_main.py
@@ -6,18 +6,59 @@ class gugu(Canvas):
         self.root.title(title)
         super(gugu,self).__init__(self.root,width=_width,height=_height,bg=color)
         self.pack()
+        self.items={}
+        self.items2=[]
+
+    def update(self):
+        self.root.update()
     
     def mainloopp(self):
-        self.root.mainloop()
+        while True:
+            self.update()
 
     def newRec(self,Position,color):
-        return self.create_rectangle(Position[0],Position[1],Position[2],Position[3],fill=color)
+        return Rec(self,Position,color)
 
     def newText(self,position,_text,color,size):
-        return self.create_text(position[0],position[1],text=_text,fill=color,font=('Arial',size))
+        return Text(self,position,_text,color,size)
     
     def newLabel(self,position,text,bgcolor,color,size,width=0,height=0):
+        return Label(self,position,text,bgcolor,color,size,width,height)
+        
+
+class Rec:
+    def __init__(self,gugu,Position,color):
+        self.gugu=gugu
+        self.item=self.gugu.create_rectangle(Position[0],Position[1],Position[2],Position[3],fill=color)
+
+    def delete(self):
+        self.gugu.delete(self.item)
+    
+    def update(self):
+        print('ok')
+
+class Text:
+    def __init__(self,gugu,position,_text,color,size):
+        self.gugu=gugu
+        self.item=self.gugu.create_text(position[0],position[1],text=_text,fill=color,font=('Arial',size))
+
+    def delete(self):
+        self.gugu.delete(self.item)
+        
+    def update(self):
+        print('ok')
+
+class Label:
+    def __init__(self,gugu,position,text,bgcolor,color,size,width=0,height=0):
+        self.gugu=gugu
         if bgcolor!=None:
             self.position=[position[0]-width,position[1]-height,position[0]+width,position[1]+height]
-            self.newRec(self.position,bgcolor)
-        self.newText(position,text,color,size)
+            self.kkk=self.gugu.newRec(self.position,bgcolor)
+        else:
+            self.kkk=None
+        self.kkkd=self.gugu.newText(position,text,color,size)
+
+    def update(self):
+        if self.kkk!=None:
+            self.kkk.update()
+        self.kkkd.update()
