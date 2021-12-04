7cfe76fb90b63892ed3e0fcd70f13322c551b6a2
link0233 <054656-106144@stu.mlc.edu.tw>
2021-12-05 07:38:45 +0800

    完成標提

--- main_screen/title.py
+++ main_screen/title.py
@@ -1,5 +1,5 @@
 class mainScreenTitle:
     def __init__(self,gugu):
         self.gugu=gugu
-        self.item=self.gugu.newLabel((320,100),"射飛機",'#ff0000','#ffffff',50,width=100,height=50)
+        self.item=self.gugu.newLabel((320,100),"射飛機",None,'#ffffff',50,width=100,height=50)
         self.item.update()
