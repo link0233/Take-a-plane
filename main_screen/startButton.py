df413f8dae417aef43302f25711d1b29d93f1a68
link0233 <054656-106144@stu.mlc.edu.tw>
2021-12-05 07:41:59 +0800

    完成繼續遊戲按鈕

--- main_screen/startButton.py
+++ main_screen/startButton.py
@@ -1,10 +1,10 @@
 class mainScreen_StartButton:
     def __init__(self,gugu):
         self.gugu=gugu
-        self.item=self.gugu.newButton([20,20,100,50],'hello','#ffffff','#000000','#00ff00',13,self.dk)
+        self.item=self.gugu.newButton([220,350,420,400],'繼續遊戲','#ffffff','#000000','#00ff00',13,self.dk)
 
     def update(self):
         self.item.update(self.gugu.key)
 
     def dk(self):
-        print(00)
+        pass
